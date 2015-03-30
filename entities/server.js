a52b508460fd778dee2eb6dbc2f962b26fb04124
Itoh, Masaru <masaru@borderless-japan.com>
2015-03-30 18:59:25 +0900

    allow find() options on entity sub

--- entities/server.js
+++ entities/server.js
@@ -1,12 +1,15 @@
 /**
- * Publication of the entitites 
- * 
+ * Publication of the entitites
+ *
  * @param  string name The name of the entity
  * @param  object options Passed to the find() method
  */
-Meteor.publish('entity', function(name, options) {
-	options = options ? options : {};
-	return orion.entities[name].collection.find(options);
+Meteor.publish('entity', function(name, query, options) {
+	check(name, String)
+	check(query, Match.Optional(Object))
+	check(options, Match.Optional(Object))
+	query = query ? query : {};
+	return orion.entities[name].collection.find(query, options);
 });
 
 /**
