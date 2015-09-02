25482295220be7ef86f3fd2f24e30a72ddfb0c8e
Itoh, Masaru <masaru@borderless-japan.com>
2015-09-02 17:40:27 +0900

    subscribe to non-i18n entities in admin

--- main/common.js
+++ main/common.js
@@ -6,7 +6,4 @@ orion = {};
 /**
  * Initializes the subscriptions mannager
  */
-if (TAPi18n.subscribe) {
-  var subsOptions = {subscribe: _.bind(TAPi18n.subscribe, TAPi18n)};
-}
-orion.subs = new SubsManager(subsOptions);
+orion.subs = new SubsManager();
